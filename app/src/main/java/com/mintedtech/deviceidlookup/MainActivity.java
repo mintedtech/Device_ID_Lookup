dd00c39adb542062cc127c5317bb227b1088d431
SA <mintedtech@gmail.com>
2021-07-19 16:45:28 -0400

    Change FAB to Copy Device ID to Clipboard

--- app/src/main/java/com/mintedtech/deviceidlookup/MainActivity.java
+++ app/src/main/java/com/mintedtech/deviceidlookup/MainActivity.java
@@ -1,9 +1,13 @@
 package com.mintedtech.deviceidlookup;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.mintedtech.deviceidlookup.databinding.ActivityMainBinding;
@@ -19,19 +23,35 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate (savedInstanceState);
 
-        binding = ActivityMainBinding.inflate (getLayoutInflater ());
-        setContentView (binding.getRoot ());
+        setContentView ();
 
         setSupportActionBar (binding.toolbar);
 
+        binding.fab.setOnClickListener (view -> copyToClipboard ());
+
+        outputDeviceID ();
+    }
+
+    private void setContentView ()
+    {
+        binding = ActivityMainBinding.inflate (getLayoutInflater ());
+        setContentView (binding.getRoot ());
+    }
+
+    private void outputDeviceID ()
+    {
         String deviceID = Settings.Secure.getString(this.getContentResolver(),
                                                     Settings.Secure.ANDROID_ID);
-        binding.contentMain.etDeviceId.setText (deviceID);
+        binding.contentMain.tvDeviceId.setText (deviceID);
+    }
 
-        binding.fab.setOnClickListener (view ->
-                                                Snackbar.make (view,
-                                                               "The Device ID is shown above.",
-                                                               Snackbar.LENGTH_LONG).show ());
+    private void copyToClipboard()
+    {
+        String deviceId = binding.contentMain.tvDeviceId.getText ().toString ();
+        ClipboardManager cm = (ClipboardManager)getApplicationContext().getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData cData = ClipData.newPlainText("Device ID", deviceId);
+        cm.setPrimaryClip (cData);
+        Toast.makeText(getApplicationContext(), "Copied the Device ID to the Clipboard", Toast.LENGTH_SHORT).show();
     }
 
     @Override
